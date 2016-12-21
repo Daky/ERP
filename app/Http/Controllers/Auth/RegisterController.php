ea20fdd31a0b3af43e4e11a404abde6cae6bc406
Kenny Lee <kenny.lee@language-center.com.tw>
2016-12-22 00:04:42 +0800

    Change laravel app:name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace ERP\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use ERP\User;
+use ERP\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
--- config/app.php
+++ config/app.php
@@ -172,11 +172,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        ERP\Providers\AppServiceProvider::class,
+        ERP\Providers\AuthServiceProvider::class,
+        // ERP\Providers\BroadcastServiceProvider::class,
+        ERP\Providers\EventServiceProvider::class,
+        ERP\Providers\RouteServiceProvider::class,
 
     ],
 
