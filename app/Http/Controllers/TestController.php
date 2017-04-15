7902a9175b3c131d92172985f3c78d3b50167991
王維新 <ga013077@gmail.com>
2017-04-15 14:26:36 +0800

    Test change save

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,36 @@
 namespace ERP\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AWS;
 
 class TestController extends Controller
 {
     //
+    public function test()
+    {
+    	$s3 = AWS::createClient('s3');
+		$file = "test.jpg";
+		$file_url = $s3->getObjectUrl(env('AWS_BUCKET'), $file);
+
+		$client = AWS::createClient('rekognition');
+		$result = $client->getEndpoint();
+		
+		dd($result);
+		/*$result = $client->compareFaces([
+    'SimilarityThreshold' => 90,
+    'SourceImage' => [
+        'S3Object' => [
+            'Bucket' => env('AWS_BUCKET'),
+            'Name' => 'test.jpg',
+        ],
+    ],
+    'TargetImage' => [
+        'S3Object' => [
+            'Bucket' => env('AWS_BUCKET'),
+            'Name' => 'test.jpg',
+        ],
+    ],
+]);
+    	dd($result);*/
+    }
 }
